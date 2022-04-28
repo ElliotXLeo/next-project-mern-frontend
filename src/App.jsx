fed33bf89d162fcad3593a6817c9b6188cc46e14
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-28 12:54:16 -0500

    Routes, pages, layout iniciales

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import AuthLayout from './layouts/AuthLayout';
+import Confirm from './pages/Confirm';
+import Login from './pages/Login';
+import NewPassword from './pages/NewPassword';
+import RecoverPassword from './pages/RecoverPassword';
+import Register from './pages/Register';
+
 function App() {
   return (
-    <h1>
-      Next Project Mern
-    </h1>
+    <Router>
+      <Routes>
+        <Route path='/' element={<AuthLayout />}>
+          <Route index element={<Login />} />
+          <Route path='register' element={<Register />} />
+          <Route path='recover-password' element={<RecoverPassword />} />
+          <Route path='recover-password/:token' element={<NewPassword />} />
+          <Route path='confirm/:id' element={<Confirm />} />
+        </Route>
+        <Route path="*" element={<h1 className="text-8xl font-semibold text-yellow-300 text-center">404</h1>} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const AuthLayoutt = () => {
+  return (
+    <section>
+      <h1>AuthLayoutt</h1>
+      <Outlet />
+    </section>
+  );
+}
+
+export default AuthLayoutt;
