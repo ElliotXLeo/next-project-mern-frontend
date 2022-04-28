b5df9e3d0aac0936d6fc4b9eda24ee2dc6cb0667
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-28 16:33:19 -0500

    Update page RecoverPassword

--- src/pages/RecoverPassword.jsx
+++ src/pages/RecoverPassword.jsx
@@ -1,5 +1,41 @@
+import { Link } from "react-router-dom";
+
 const RecoverPassword = () => {
-  return (<div>RecoverPassword</div>);
+  return (
+    <section className="py-8 md:py-16">
+      <div className="flex flex-col gap-8 container mx-auto px-8 md:px-3">
+        <h1 className="text-sky-600 text-4xl font-black text-center">
+          Recupera tu acceso y no pierdas tus <span className="text-slate-700">proyectos</span>
+        </h1>
+        <form action="" className="flex flex-col gap-4 bg-white border rounded-lg w-4/5 max-w-sm mx-auto p-4">
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            className="w-full border rounded-md p-2"
+            required />
+          <input
+            type="submit"
+            value="Recuperar"
+            className="bg-sky-700 text-white font-bold rounded-md p-2 cursor-pointer transition-colors hover:bg-sky-800" />
+        </form>
+
+        <nav className="flex items-center justify-between w-4/5 max-w-sm mx-auto">
+          <Link
+            to="/"
+            className="text-slate-500 text-sm font-bold transition-colors hover:text-slate-800"
+          >
+            Ya tengo cuenta
+          </Link>
+          <Link
+            to="/register"
+            className="text-slate-500 text-sm font-bold transition-colors hover:text-slate-800"
+          >
+            No tengo cuenta
+          </Link>
+        </nav>
+      </div>
+    </section>);
 }
 
 export default RecoverPassword;
