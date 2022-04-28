c87bdb022b8b146f2d60894f0a2eebad7dfdf089
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-28 17:31:48 -0500

    Update page Register estado

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,37 +1,77 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+
+  const [user, setUser] = useState({
+    nombre: '',
+    email: '',
+    password: '',
+    repeatedPassword: ''
+  });
+
+  const { nombre, email, password, repeatedPassword } = user;
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.id]: e.target.value
+    });
+  };
+
+  const handleSubtmit = async (e) => {
+    e.preventDefault();
+    if ([nombre, email, password, repeatedPassword].includes('')) {
+      console.log('Todos los campos son obligatorios');
+    }
+  };
+
   return (
     <section className="py-8 md:py-16">
       <div className="flex flex-col gap-8 container mx-auto px-8 md:px-3">
         <h1 className="text-sky-600 text-4xl font-black text-center">
           Registrate y administra tus <span className="text-slate-700">proyectos</span>
         </h1>
-        <form action="" className="flex flex-col gap-4 bg-white border rounded-lg w-4/5 max-w-sm mx-auto p-4">
+        <form
+          onSubmit={handleSubtmit}
+          className="flex flex-col gap-4 bg-white border rounded-lg w-4/5 max-w-sm mx-auto p-4"
+        >
           <input
             type="text"
             id="nombre"
             placeholder="Nombre"
             className="w-full border rounded-md p-2"
-            required />
+            value={nombre}
+            onChange={handleChange}
+          />
+          {/* required /> */}
           <input
             type="email"
             id="email"
             placeholder="Email"
             className="w-full border rounded-md p-2"
-            required />
+            value={email}
+            onChange={handleChange}
+          />
+          {/* required /> */}
           <input
             type="password"
             id="password"
             placeholder="Password"
             className="w-full border rounded-md p-2"
-            required />
+            value={password}
+            onChange={handleChange}
+          />
+          {/* required /> */}
           <input
             type="password"
             id="repeatedPassword"
             placeholder="Repetir password"
             className="w-full border rounded-md p-2"
-            required />
+            value={repeatedPassword}
+            onChange={handleChange}
+          />
+          {/* required /> */}
           <input
             type="submit"
             value="Registrar"
