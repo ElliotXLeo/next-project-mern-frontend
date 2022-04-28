0b1d1f8dd97393ccf7fe169e3da862b603fcb433
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-28 16:48:27 -0500

    Update page Confirm

--- src/pages/Confirm.jsx
+++ src/pages/Confirm.jsx
@@ -1,5 +1,13 @@
 const Confirm = () => {
-  return (<div>Confirm</div>);
+  return (
+    <section className="py-8 md:py-16">
+      <div className="flex flex-col gap-8 container mx-auto px-8 md:px-3">
+        <h1 className="text-sky-600 text-4xl font-black text-center">
+          Confirma tu cuenta y administra tus <span className="text-slate-700">proyectos</span>
+        </h1>
+      </div>
+    </section>
+  );
 }
 
 export default Confirm;
