85816e69c6f2e178f81a7870080b960fa9d41cf9
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-29 17:46:24 -0500

    Update page Login autenticación

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,97 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Alert from "../components/sections/Alert";
+import axiosInstance from "../config/axiosInstance";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  });
+
+  const [alert, setAlert] = useState({
+    message: '',
+    error: false
+  });
+
+  const { email, password } = user;
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.id]: e.target.value
+    });
+  };
+
+  const handleSubtmit = async (e) => {
+    e.preventDefault();
+    if ([email, password].includes('')) {
+      setAlert({
+        message: 'Todos los campos son obligatorios',
+        error: true
+      });
+    } else if (password.length < 6) {
+      setAlert({
+        message: 'El password debe contener al menos 6 caracteres',
+        error: true
+      });
+    } else {
+      try {
+        const method = 'POST';
+        const resource = '/users/login';
+        const options = {
+          method,
+          url: resource,
+          data: user
+        };
+        const { data } = await axiosInstance(options);
+        localStorage.setItem('token', data.token);
+        setUser({
+          email: '',
+          password: ''
+        });
+        setAlert({
+          message: `Bienvenido ${data.nombre}`,
+          error: false
+        });
+      } catch (error) {
+        setAlert({
+          message: error.response.data.message,
+          error: true
+        });
+      }
+    }
+  };
+
   return (
     <section className="py-8 md:py-16">
       <div className="flex flex-col gap-8 container mx-auto px-8 md:px-3">
         <h1 className="text-sky-600 text-4xl font-black text-center">
           Inicia sesión y administra tus <span className="text-slate-700">proyectos</span>
         </h1>
-        <form action="" className="flex flex-col gap-4 bg-white border rounded-lg w-4/5 max-w-sm mx-auto p-4">
+        <form
+          onSubmit={handleSubtmit}
+          className="flex flex-col gap-4 bg-white border rounded-lg w-4/5 max-w-sm mx-auto p-4"
+        >
           <input
             type="email"
             id="email"
             placeholder="Email"
             className="w-full border rounded-md p-2"
-            required />
+            value={email}
+            onChange={handleChange}
+            required
+          />
           <input
             type="password"
             id="password"
             placeholder="Password"
             className="w-full border rounded-md p-2"
-            required />
+            value={password}
+            onChange={handleChange}
+            required
+            minLength="6"
+          />
           <input
             type="submit"
             value="Iniciar Sesión"
@@ -40,6 +112,9 @@ const Login = () => {
             Olvidé mi password
           </Link>
         </nav>
+        {
+          alert.message && <Alert alert={alert} />
+        }
       </div>
     </section>
   );
