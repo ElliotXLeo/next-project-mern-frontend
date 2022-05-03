865f4f36c81408732f3baeab69164e34dcdda46b
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-03 16:41:29 -0500

    Update component Sidebar

--- src/components/sections/Sidebar.jsx
+++ src/components/sections/Sidebar.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 const Sidebar = () => {
+  const { auth } = useAuth();
+
   return (
     <nav className="flex flex-col gap-4 p-10 md:w-80">
-      <p className="text-xl font-bold">Hola: Elliot</p>
+      <p className="text-xl font-bold">Hola: {auth.name}</p>
       <Link
         to="create-project"
         className="bg-sky-600 rounded-md text-white text-sm text-center font-bold uppercase p-2"
