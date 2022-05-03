84b1f71d9e98e7133f83b66dfe45bd65c104efb2
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-03 16:15:36 -0500

    Update component Header

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return (<header>Header</header>);
+  return (
+    <header className="py-4 bg-white border-b">
+      <div className="flex justify-between container mx-auto">
+        <h2 className="text-2xl text-sky-600 font-black">NextProject</h2>
+        <input type="search" placeholder="Buscar proyectos" className="p-2 border rounded-lg hidden md:flex md:w-96" />
+        <div className="flex items-center gap-4">
+          <Link to="/projects" className="font-bold uppercase">Proyectos</Link>
+          <button className="bg-sky-600 rounded-md text-white text-sm font-bold uppercase p-2">
+            Cerrar Sesión
+          </button>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
