740a897f4f3555d7d6d5d33ee2e521ee8bdbdc30
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-04 14:51:27 -0500

    Update component Header

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="py-4 bg-white border-b">
       <div className="flex items-center justify-between container mx-auto">
-        <h2 className="text-2xl text-sky-600 font-black">NextProject</h2>
+        <Link to="/projects" className="text-2xl text-sky-600 font-black">NextProject</Link>
         <input type="search" placeholder="Buscar proyectos" className="p-2 border rounded-lg hidden md:flex md:w-96" />
         <div className="flex items-center gap-4">
           <Link to="/projects" className="font-bold uppercase">Proyectos</Link>
