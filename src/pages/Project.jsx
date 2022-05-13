de6ffecad47bc442de2cc135c3a9f6e03b24d422
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-13 17:13:21 -0500

    Update page Project

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -26,85 +26,87 @@ const Project = () => {
     readProject(id);
   }, []);
 
-  return (
-    <>
-      {
-        loading ?
-          <Loading />
-          :
-          <section className="flex flex-col gap-8">
-            <header className="flex items-center justify-between">
-              <h2 className="text-4xl font-black">{name}</h2>
-              <div className="flex items-center gap-4">
-                <Link to={`/projects/update/${id}`} className="transition-all hover:sepia">
-                  ✏
-                </Link>
-                <button
-                  className="transition-all hover:sepia"
-                  onClick={handleClick}
-                >
-                  🗑
-                </button>
-              </div>
-            </header>
-            <section className="flex flex-col gap-4">
-              <button
-                onClick={handleFormModalTask}
-                className="bg-sky-400 rounded-md text-white font-bold p-2 cursor-pointer transition-colors hover:bg-sky-500 w-full"
-              >
-                ➕ Nueva tarea
-              </button>
-              <h3 className="text-xl font-bold text-center">Tareas</h3>
-              <div className="bg-white shadow rounded-lg p-4">
-                {
-                  project.tasks?.length ?
-                    project.tasks?.map((element) => {
-                      return (
-                        <TasksTask
-                          key={element._id}
-                          task={element}
-                        />
-                      );
-                    })
-                    :
-                    <p className="text-center">No hay tareas</p>
-                }
-              </div>
-              <div className="flex items-center justify-between">
-                <h3 className="text-xl font-bold text-center">Desarrolladores</h3>
-                <Link
-                  to={`/projects/add-developer/${project._id}`}
-                  className="text-gray-400 uppercase font-bold transition-colors hover:text-black"
-                >
-                  Agregar
-                </Link>
-              </div>
-              <div className="bg-white shadow rounded-lg p-4">
-                {
-                  project.developers?.length ?
-                    project.developers?.map((element) => {
-                      return (
-                        <DevelopersDeveloper
-                          key={element._id}
-                          developer={element}
-                        />
-                      );
-                    })
-                    :
-                    <p className="text-center">No hay desarrolladores</p>
-                }
-              </div>
-              {
-                alert.message && <Alert alert={alert} />
-              }
-              <FormModal />
-              <TasksDeleteModal />
-              <DevelopersDeleteModal />
-            </section>
-          </section>
-      }
-    </>
-  );
+  if (loading) {
+    return <Loading />
+  } else if (project._id) {
+    return (
+      <section className="flex flex-col gap-8">
+        <header className="flex items-center justify-between">
+          <h2 className="text-4xl font-black">{name}</h2>
+          <div className="flex items-center gap-4">
+            <Link to={`/projects/update/${id}`} className="transition-all hover:sepia">
+              ✏
+            </Link>
+            <button
+              className="transition-all hover:sepia"
+              onClick={handleClick}
+            >
+              🗑
+            </button>
+          </div>
+        </header>
+        <section className="flex flex-col gap-4">
+          <button
+            onClick={handleFormModalTask}
+            className="bg-sky-400 rounded-md text-white font-bold p-2 cursor-pointer transition-colors hover:bg-sky-500 w-full"
+          >
+            ➕ Nueva tarea
+          </button>
+          <h3 className="text-xl font-bold text-center">Tareas</h3>
+          <div className="bg-white shadow rounded-lg p-4">
+            {
+              project.tasks?.length ?
+                project.tasks?.map((element) => {
+                  return (
+                    <TasksTask
+                      key={element._id}
+                      task={element}
+                    />
+                  );
+                })
+                :
+                <p className="text-center">No hay tareas</p>
+            }
+          </div>
+          <div className="flex items-center justify-between">
+            <h3 className="text-xl font-bold text-center">Desarrolladores</h3>
+            <Link
+              to={`/projects/add-developer/${project._id}`}
+              className="text-gray-400 uppercase font-bold transition-colors hover:text-black"
+            >
+              Agregar
+            </Link>
+          </div>
+          <div className="bg-white shadow rounded-lg p-4">
+            {
+              project.developers?.length ?
+                project.developers?.map((element) => {
+                  return (
+                    <DevelopersDeveloper
+                      key={element._id}
+                      developer={element}
+                    />
+                  );
+                })
+                :
+                <p className="text-center">No hay desarrolladores</p>
+            }
+          </div>
+          {
+            alert.message && <Alert alert={alert} />
+          }
+          <FormModal />
+          <TasksDeleteModal />
+          <DevelopersDeleteModal />
+        </section>
+      </section>
+    );
+  } else {
+    return <Alert alert={{
+      message: 'Acceso no autorizado',
+      error: true
+    }} />
+  }
 }
 
 export default Project;
