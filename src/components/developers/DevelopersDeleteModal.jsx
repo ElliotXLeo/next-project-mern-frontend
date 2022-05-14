2433d695b175d51e9ca441d933fe8f23c5915056
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-14 18:51:18 -0500

    Update component DevelopersDeleteModal

--- src/components/developers/DevelopersDeleteModal.jsx
+++ src/components/developers/DevelopersDeleteModal.jsx
@@ -10,7 +10,7 @@ const DevelopersDeleteModal = () => {
   return (
     <Transition.Root show={developerDeleteModal} as={Fragment}>
       <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={handleDeveloperDeleteModal}>
-        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="flex items-center justify-center min-h-screen p-4 px-4 text-center sm:block sm:p-0">
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
